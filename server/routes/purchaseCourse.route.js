582f4a0e39ac7a2e3092913d14a09503fea0c7b3
jack reider <jackreider997@gmail.com>
2025-04-11 17:43:51 +0100

    Update purchaseCourse.route.js

--- server/routes/purchaseCourse.route.js
+++ server/routes/purchaseCourse.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { createCheckoutSession, getAllPurchasedCourse, getCourseDetailWithPurchaseStatus, stripeWebhook } from "../controllers/coursePurchase.controller.js";
+import { createCheckoutSession, getAllPurchasedCourse, getCourseDetailWithPurchaseStatus, konnectWebhook } from "../controllers/coursePurchase.controller.js";
 
 const router = express.Router();
 
 router.route("/checkout/create-checkout-session").post(isAuthenticated, createCheckoutSession);
-router.route("/webhook").post(express.raw({type:"application/json"}), stripeWebhook);
+router.route("/webhook").post(express.raw({type:"application/json"}), konnectWebhook);
 router.route("/course/:courseId/detail-with-status").get(isAuthenticated,getCourseDetailWithPurchaseStatus);
 
 router.route("/").get(isAuthenticated,getAllPurchasedCourse);
